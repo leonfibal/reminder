f6b03cf7c79ccf747d49c789ba1cf2cbc2fea597
Leon Zawodowiec <Leon Zawodowiec>
2016-07-02 19:14:14 +0200

    api project

---
+++ src/Reminder.Api/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Reminder.Api.Controllers
+{
+    [RoutePrefix("test")]
+    public class TestController : ApiController
+    {
+
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult Test()
+        {
+            return Ok(new
+            {
+                Message = "I am alive!",
+                Date = DateTime.Now
+            });
+        }
+    }
+}
---
+++ src/Reminder.Api/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+using Microsoft.Owin;
+using Newtonsoft.Json.Serialization;
+using Owin;
+
+[assembly: OwinStartup(typeof(Reminder.Api.Startup))]
+
+namespace Reminder.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            app.UseWebApi(config);
+        }
+    }
+}
